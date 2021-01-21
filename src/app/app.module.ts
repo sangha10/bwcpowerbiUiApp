73e0a30ae5197f934999a6f1b589cc6857449363
sangha10 <34095168+sangha10@users.noreply.github.com>
2021-01-21 20:32:21 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { AppComponent } from './app.component'; 
+import { ReportComponent } from './report/report.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MsAdalAngular6Module } from 'microsoft-adal-angular6';
+import { AuthenticationGuard } from 'microsoft-adal-angular6';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ReportService } from './Services/report.service'; 
+import { environment } from './../environments/environment';
+//import { NgxSpinnerModule } from "ngx-spinner";  
 
-import { AppComponent } from './app.component';
+const routes: Routes = [
+  { path: 'report', component: ReportComponent, pathMatch: 'full', canActivate: [AuthenticationGuard] }, 
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,   
+    RouterModule.forRoot(routes),
+    //RouterModule.forRoot(routes, {useHash: true}),
+    MsAdalAngular6Module.forRoot(getAdalConfig)//,
+    //NgxSpinnerModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthenticationGuard, ReportService 
+  ],
+  bootstrap: [AppComponent]//,
+  //schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
+
+export function getAdalConfig() {
+  return {
+    tenant: environment.adalConfig.tenant,
+    clientId: environment.adalConfig.clientId,
+    cacheLocation: environment.adalConfig.cacheLocation,
+    endpoints: { "api": environment.adalConfig.endPoint },
+    navigateToLoginRequestUrl: environment.adalConfig.navigateToLoginRequestUrl,
+    redirectUri: environment.adalConfig.redirectUri,
+    expireOffsetSeconds: environment.adalConfig.expireOffsetSeconds
+  };
+}
