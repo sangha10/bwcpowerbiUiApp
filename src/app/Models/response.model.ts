5a77cf9ad04be976da02ba8ca2f82d0a271b8d65
sangha10 <34095168+sangha10@users.noreply.github.com>
2021-01-21 20:50:11 +0530

    Create response.model.ts

---
+++ src/app/Models/response.model.ts
@@ -0,0 +1,11 @@
+export class Response {
+    reportName: string;
+    embedUrl: string;
+    embedToken: string;
+    tokenId: string;
+    expiration: string;
+}
+
+export class TokenResponse {
+    token: string; 
+}
